873039ed9fe2ac9bfaeb4a16680875c846b71229
cahyaadi11 <61812305+cahyaadi11@users.noreply.github.com>
2020-04-30 21:10:56 +0800

    Video Part 9
    perubahan pada index.js dan today.html

---
+++ Part 9/index.js
@@ -0,0 +1,159 @@
+const electron = require("electron");
+const { v4 : uuidv4 } = require('uuid');
+uuidv4();
+
+const {
+    app, 
+    BrowserWindow, 
+    Menu, 
+    ipcMain
+} = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+let aboutWindow;
+
+let allApointment = [];
+
+app.on("ready", () => {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }, 
+        title : "Aplikasi Kedokteran"
+    });
+
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+
+        app.quit();
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", ()  => (listWindow = null));
+};
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", ()  => (createWindow = null));
+};
+
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow ({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "About"
+    });
+
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`file://${__dirname}/agus.html`);
+    aboutWindow.on("closed", ()  => (caboutWindow = null));
+};
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    appointment["id"] = uuidv4();
+    appointment["done"] = 0;
+    allApointment.push(appointment);
+    sendTodayAppointments();
+    createWindow.close();
+
+    console.log(allApointment);
+});
+
+ipcMain.on("appointment:request:list", event => {
+    listWindow.webContents.send('appointment:response:list', allApointment);
+});
+
+ipcMain.on("appointment:request:today", event => {
+    sendTodayAppointments();
+    console.log("here2");
+});
+
+ipcMain.on("appointment:done", (event, id) => {
+    allApointment.forEach(appointment => {
+        appointment.done = 1
+    })
+
+    sendTodayAppointments()
+});
+
+const sendTodayAppointments = () => {
+    const today = new Date().toISOString().slice(0,10);
+    const filtered = allApointment.filter(
+        appointment => appointment.date === today
+    );
+
+    todayWindow.webContents.send("appointment:response:today", filtered);
+}
+
+const menuTemplate =  [{
+        label: "File",
+        submenu:[{
+                label: "New Appointment",
+
+                click() {
+                    createWindowCreator()
+                }
+            },
+            {
+                label: "All Apointments",
+                click() {
+                    listWindowCreator();
+                }
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl + Q",
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+
+    {
+
+        label: "View",
+        submenu: [{ role: "reload" }, { role: "toggledevtools" }]
+    }
+    ,
+    {
+
+        label: "About",
+        click(){
+            aboutWindowCreator();
+}
+    }
+
+
+]
